09c4e3b3138d619a310b77163120dc5726f10d6b
Darien <dhayes@CIS-04.CIS.com>
2015-03-09 07:49:56 -0500

    Darien Hayes

---
+++ Challenge2.py
@@ -0,0 +1,30 @@
+#Darien Hayes
+#Challenge 2
+
+from random import randint
+
+class Player1(object):
+    """Player 1's random card"""
+    def hand1(self, name):
+        name = input("Name? ")
+        print("The card", Name , "got is ")
+        print(randint(0,100))
+        return;
+
+class Player2(object):
+    """Player 2's random card"""
+    def hand2(self, name):
+        name = input("Name? ")
+        print("The card", Name , "got is ")
+        print(randint(0,100))
+        return;
+    
+#main
+print("Welcome to War.")
+
+Player1 = Player1()
+print(Player1)
+Player2 = Player2()
+print(Player2)
+
+input("Press the enter key to exit.") 
---
+++ Chellenge4.py
@@ -0,0 +1,6 @@
+#Darien Hayes
+#Challenge 4
+
+class Wonderland(object):
+    def Miss_Haven(self, location):
+        
